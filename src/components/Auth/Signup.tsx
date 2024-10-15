08e8b9b6ab8da4d2b3d12f9a640969d7d852ac41
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2024-10-15 09:45:01 +0900

    アカウント登録機能の作成

---
+++ src/components/Auth/Signup.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { auth } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const Signup: any = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSignup = async (e: any) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      alert("User registered successfully");
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <div style={{ maxWidth: "600px", margin: "0 auto" }}>
+        <form onSubmit={handleSignup}>
+          <div>
+            <label>Email</label>
+            <div>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          <div>
+            <label>Password</label>
+            <div>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          <div>
+            <label>ConfirmPassword</label>
+            <div>
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                placeholder="ConfirmPassword"
+              />
+            </div>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <button type="submit">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
