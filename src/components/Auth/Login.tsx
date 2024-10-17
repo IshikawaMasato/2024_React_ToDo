126d94887969935cca7960989c7e29eea66a5a8e
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-10-17 21:22:28 +0900

    ログイン機能の作成

---
+++ src/components/Auth/Login.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { auth } from "../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import "bulma/css/bulma.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("User logged in successfully");
+      navigate("/TodoList");
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <div className="container is-fluid">
+      <div className="box" style={{ maxWidth: "600px", margin: "0 auto" }}>
+        <form onSubmit={handleLogin}>
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input
+                className="input"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Password</label>
+            <div className="control">
+              <input
+                className="input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          <div className="field has-text-centered">
+            <div className="control">
+              <button className="button is-primary" type="submit">
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
