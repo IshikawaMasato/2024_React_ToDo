a67bc896a2b857568b9907b6cb9560c034623b0f
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2024-10-18 13:54:49 +0900

    Todoの追加機能の作成

--- src/components/ToDo/ToDoList.tsx
+++ src/components/ToDo/ToDoList.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Logout from "../Auth/Logout"; // Logoutコンポーネントをインポート
+import AddTodo from "./AddToDo";
 
 const ToDoList: React.FC = () => {
   return (
     <div>
+      タスク登録
+      <AddTodo />
       <h1>一覧画面</h1>
       <Logout /> {/* ログアウトボタンを追加 */}
     </div>
