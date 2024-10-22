7c5dec30425dadc5fe728ab9d451c1d12586fcf5
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-10-22 10:39:30 +0900

    ToDoリスト取得

--- src/components/ToDo/ToDoList.tsx
+++ src/components/ToDo/ToDoList.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 import Logout from "../Auth/Logout"; // Logoutコンポーネントをインポート
+import { useEffect, useState } from "react";
+import { auth, db } from "../../firebase";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 const ToDoList: React.FC = () => {
+  const [todos, setTodos] = useState([]);
+  const [editingTodoId, setEditingTodoId] = useState(null);
+
+  const fetchTodos = async () => {
+    const user = auth.currentUser;
+    if (user) {
+      const q = query(
+        collection(db, "todos"),
+        where("user_id", "==", user.uid)
+      );
+      const querySnapshot = await getDocs(q);
+      setTodos(
+        querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      );
+    }
+  };
   return (
     <div>
       <h1>一覧画面</h1>
