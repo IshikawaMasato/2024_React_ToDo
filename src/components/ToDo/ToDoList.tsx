b9f6e754a39f6d1a73e59f3e562983f49449d388
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2024-10-23 10:33:32 +0900

    CompleteTodoの作成

--- src/components/ToDo/ToDoList.tsx
+++ src/components/ToDo/ToDoList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Logout from "../Auth/Logout"; // Logoutコンポーネントをインポート
 import EditTodo from "./EditTodo";
+import DeleteTodo from "./DeleteTodo";
 import { auth, db } from "../../firebase";
 import { collection, query, where, getDocs } from "firebase/firestore";
 
